f02fc20a80b751eb6bb62bb4eb29d4b8b764bc85
Евгений Александрович Федосеев <evgeniy1294@yandex.ru>
2021-10-05 16:23:26 +0300

    Work on GUI

--- src/session_widget.cpp
+++ src/session_widget.cpp
@@ -1,9 +1,11 @@
 #include <QHBoxLayout>
 #include <QSplitter>
+#include <QTextBrowser>
 #include <QLabel>
+
 #include "session_widget.h"
 #include "tools_widget.h"
-
+#include "log_widget.h"
 
 SessionWidget::SessionWidget(QWidget *parent)
   : QWidget(parent)
@@ -23,22 +25,10 @@ SessionWidget::~SessionWidget()
 void SessionWidget::createGui()
 {
   auto mLayout = new QHBoxLayout(this);
+    auto tools_wdg   = new ToolsWidget();
+    auto log_wdg     = new LogWidget();
 
-    mSplitter = new QSplitter();
-    mSplitter->setOrientation(Qt::Horizontal);
-    mSplitter->setCollapsible(0, true);
-//    mSplitter->setHandleWidth(5);
-//    mSplitter->setStyleSheet("QSplitter::handle {background: red;}");
-//    mSplitter->setChildrenCollapsible(true);
-
-//      mLabel = new QLabel(tr("Im label!"));
-      auto tools_wdg = new ToolsWidget();
-      auto mSplitterLabel2 = new QLabel(tr("Im splitter!"));
-      mSplitterLabel2->setStyleSheet("QLabel {background: red;}");
-
-//    mSplitter->addWidget(mLabel);
-    mSplitter->addWidget(tools_wdg);
-    mSplitter->addWidget(mSplitterLabel2);
-
-  mLayout->addWidget(mSplitter);
+      mLayout->setAlignment(Qt::AlignTop);
+      mLayout->addWidget(tools_wdg, 0);
+      mLayout->addWidget(log_wdg, 1);
 }
