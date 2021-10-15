e3ce9d5d7b8c73a5a23ccba7f3cf2633a33104e1
Evgenii Fedoseev <evgeniy1294@yandex.ru>
2021-10-15 09:16:10 +0300

    Test QTableWidget

--- src/session_widget.cpp
+++ src/session_widget.cpp
@@ -1,4 +1,11 @@
 #include <QLayout>
+#include <QTableWidget>
+#include <QHeaderView>
+#include <QSpinBox>
+#include <QHeaderView>
+#include <QAction>
+#include <QMenu>
+#include <QToolButton>
 #include <DockManager.h>
 
 #include "session_widget.h"
@@ -42,13 +49,57 @@ void SessionWidget::createGui()
   auto script_dock_wdg = new ads::CDockWidget("Scripting");
     script_dock_wdg->setWidget(script_wdg);
 
+  /*
   auto macro_wdg = new SequenceMultiWidget();
     connect(&Singleton::instance().mSequenceStorage, &SequenceStorage::sSeqAppended, macro_wdg, &SequenceMultiWidget::addSequence);
     connect(&Singleton::instance().mSequenceStorage, &SequenceStorage::sSeqRemoved, macro_wdg, &SequenceMultiWidget::removeSequence);
     connect(&Singleton::instance().mSequenceStorage, &SequenceStorage::sCleared, macro_wdg, &SequenceMultiWidget::clear);
+  */
+  /* -------[TEST]---------- */
+  auto mSendAct   = new QAction(QIcon(":/icons/arrow.svg"), tr("&Send"), this);
+  auto mEditAct   = new QAction(QIcon(":/icons/edit.svg") , tr("&Show in editor"), this);
+  auto mRemoveAct = new QAction(QIcon(":/icons/close.svg"), tr("&Remove"), this);
+
+  auto mToolMenu = new QMenu();
+    mToolMenu->addAction(mSendAct);
+    mToolMenu->addAction(mEditAct);
+    mToolMenu->addSeparator();
+    mToolMenu->addAction(mRemoveAct);
+
+  auto mToolButton  = new QToolButton();
+     mToolButton->setIcon(QIcon(":/icons/arrow.svg"));
+     mToolButton->setMenu(mToolMenu);
+
+  auto mRepeatTimeWgt = new QSpinBox();
+    mRepeatTimeWgt->setMaximum(10000);
+    mRepeatTimeWgt->setSingleStep(100);
+    mRepeatTimeWgt->setValue(0);
+    mRepeatTimeWgt->setSuffix("ms");
+    mRepeatTimeWgt->setSpecialValueText(QObject::tr("No Repeat"));
+
+  auto sq_table_wgt = new QTableWidget();
+    sq_table_wgt->setColumnCount(4);
+    sq_table_wgt->setRowCount(2);
+
+    //sq_table_wgt->horizontalHeader()->setStretchLastSection(true);
+
+    QHeaderView *headerView = new QHeaderView(Qt::Horizontal);
+    sq_table_wgt->setHorizontalHeader(headerView);
+    headerView->setSectionResizeMode(0, QHeaderView::Stretch);
+    headerView->setSectionResizeMode(1, QHeaderView::Stretch);
+    headerView->setSectionResizeMode(2, QHeaderView::Fixed);
+    headerView->setSectionResizeMode(3, QHeaderView::Fixed);
+    sq_table_wgt->setColumnWidth(0, 12);
+    sq_table_wgt->horizontalHeader()->hide();
+
+    sq_table_wgt->setCellWidget(0, 2, mRepeatTimeWgt);
+    sq_table_wgt->setCellWidget(0, 3, mToolButton);
+    sq_table_wgt->setColumnWidth(3, 12);
+    //sq_table_wgt->re
+    //sq_table_wgt->resizeColumnsToContents();
 
   auto macro_dock_wdg = new ads::CDockWidget("Macroses");
-    macro_dock_wdg->setWidget(macro_wdg);
+    macro_dock_wdg->setWidget(sq_table_wgt);
 
 
   mDockManager = new ads::CDockManager();
---
+++ src/sq_table_widget.cpp
@@ -0,0 +1,1 @@
+#include "sq_table_widget.h"
