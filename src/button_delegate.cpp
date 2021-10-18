002856b71749f26f783128515eda7269bd956a4d
Евгений Александрович Федосеев <evgeniy1294@yandex.ru>
2021-10-18 10:44:06 +0300

    Fix tab

--- src/button_delegate.cpp
+++ src/button_delegate.cpp
@@ -12,51 +12,51 @@ ButtonDelegate::ButtonDelegate(QObject *parent)
      : QItemDelegate(parent)
      , mState(QStyle::State_Enabled)
      , activeRow(-1)
- {
- }
+{
+}
 
 
 
- void ButtonDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
- {
-     QStyleOptionButton btn;
-     btn.rect     = option.rect;
-     btn.icon     = QIcon(":/icons/arrow.svg");
-     btn.features = QStyleOptionButton::Flat;
+void ButtonDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+    QStyleOptionButton btn;
+    btn.rect     = option.rect;
+    btn.icon     = QIcon(":/icons/arrow.svg");
+    btn.features = QStyleOptionButton::Flat;
 
-     if (option.state & QStyle::State_MouseOver) {
-         btn.state  = (activeRow == index.row()) ? mState : QStyle::State_Raised;
-     }
+    if (option.state & QStyle::State_MouseOver) {
+        btn.state  = (activeRow == index.row()) ? mState : QStyle::State_Raised;
+    }
 
-     btn.state |= QStyle::State_Enabled;
+    btn.state |= QStyle::State_Enabled;
 
-     QApplication::style()->drawControl( QStyle::CE_PushButton, &btn, painter);
- }
+    QApplication::style()->drawControl( QStyle::CE_PushButton, &btn, painter);
+}
 
 
 
- bool ButtonDelegate::editorEvent(QEvent *event, QAbstractItemModel *model, const QStyleOptionViewItem &option, const QModelIndex &index)
- {
-     switch(event->type()) {
-         case QEvent::MouseButtonPress:
-             activeRow  = index.row();
-             mState = QStyle::State_Sunken;
-             break;
+bool ButtonDelegate::editorEvent(QEvent *event, QAbstractItemModel *model, const QStyleOptionViewItem &option, const QModelIndex &index)
+{
+    switch(event->type()) {
+        case QEvent::MouseButtonPress:
+            activeRow  = index.row();
+            mState = QStyle::State_Sunken;
+            break;
 
-         case QEvent::MouseButtonDblClick:
-         case QEvent::MouseButtonRelease: {
-             activeRow = -1;
-             mState = QStyle::State_Raised;
+        case QEvent::MouseButtonDblClick:
+        case QEvent::MouseButtonRelease: {
+            activeRow = -1;
+            mState = QStyle::State_Raised;
 
-             emit triggered(index);
+            emit triggered(index);
 
-         } break;
+        } break;
 
-         default:
-              return false;
-     };
+        default:
+             return false;
+    };
 
-     return true;
- }
+    return true;
+}
 
 
