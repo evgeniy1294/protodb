b6e968c3d8160bec6c7bd46c56b6e7342d2cda27
Evgenii Fedoseev <evgeniy1294@yandex.ru>
2022-05-18 17:11:42 +0300

    update

--- libs/base/include/protodb/SessionManager.h
+++ libs/base/include/protodb/SessionManager.h
@@ -1,44 +1,72 @@
 #pragma once
 
-#include <QObject>
+#include <QAbstractTableModel>
+#include <QDateTime>
 
-class SessionManager: public QObject {
+class SessionManager: public QAbstractTableModel {
     Q_OBJECT
     Q_DISABLE_COPY(SessionManager)
 
 public:
+    enum Columns {
+        kColumnName        = 0,
+        kColumnLastChanged = 1,
+        kColumnDescription = 2,
+
+        kColumnCount
+    };
+
+public:
+    // -------------- [ Session Manager Interface ] -------------- //
     static SessionManager& instance();
 
     // Установка рабочего каталога
     bool setWorkingDirectory(const QString& path);
 
-    // Имя текущей загруженной сессии
-    QString currentSession() const;
+    // Сохранение модели
+    virtual bool saveToWorkingDirectory();
 
-    // Список сессий
-    QStringList availableSessions() const;
-
-    // Переименование сессии
-    bool renameSession(const QString& old_name, const QString& new_name);
+    // ID загруженной сессии
+    int currentSessionId() const;
 
     // Создание новой сессии или копии другой сессии
-    virtual bool createSession(const QString& name, const QString& origin = QString());
+    virtual bool createSession(const QString& name, const QString& description = QString(), const QString& origin = QString());
 
     // Удаление сессии
-    bool removeSession(const QString& name);
+    virtual bool removeSession(const QString& name);
+    virtual bool removeSession(int id);
 
     // Загрузка сессии
     bool loadLastSession();
     virtual bool loadSession(const QString& name);
+    virtual bool loadSession(int id);
 
     // Сохранение сессии
     virtual bool saveSession(const QString& name = QString());
 
+    // ------------- [ Table Model Interface ] ---------------- //
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex& parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+
 signals:
-    void sSessionAdded(QString name);
-    void sSessionRemoved(QString name);
-    void sSessionAboutToBeRemoved( const QString& prefix );
+    void sSessionsAdded(QStringList names);
+    void sSessionsRemoved(QStringList names);
+    void sSessionsAboutToBeRemoved( QStringList names );
 
 private:
     SessionManager(QObject* parent = nullptr);
+
+    struct Session {
+        QString name;
+        QString description;
+        QDateTime last_changed;
+    };
+
+private:
+    QString m_working_dir_path;
+    QString m_last_session_name;
+    int m_curr_session_id;
+    QList<Session> m_sessions;
 };
--- libs/base/src/SessionManager.cpp
+++ libs/base/src/SessionManager.cpp
@@ -1,5 +1,11 @@
 #include "protodb/SessionManager.h"
 
+#include "protodb/utils/JsonBaseUtils.h"
+
+#include <QDir>
+#include <QStandardPaths>
+#include <QCoreApplication>
+
 SessionManager& SessionManager::instance()
 {
     static SessionManager session_manager;
@@ -7,6 +13,140 @@ SessionManager& SessionManager::instance()
 }
 
 SessionManager::SessionManager(QObject *parent)
+    : QObject(this)
 {
 
+}
+
+bool SessionManager::setWorkingDirectory(const QString &path)
+{
+    if ( m_working_dir_path == path ) {
+        return true;
+    }
+
+    if ( !QDir(path).exists() ) {
+        if ( !QDir().mkdir(path) ) {
+            return false;
+        }
+    }
+
+    m_working_dir_path = path;
+
+    // Load sissions info
+    nlohmann::json sessions;
+        readFromFile(path + "/sessions.json", sessions);
+
+    if ( sessions.contains("last") ) {
+        if ( sessions["last"].is_string() ) {
+            m_last_session_name = sessions["last"].get<QString>();
+        }
+    }
+
+    if ( !sessions.contains("sessions") ) {
+        return true;
+    }
+
+     if ( !sessions["sessions"].is_array() ) {
+         return true;
+     }
+
+     // Очистка модели
+     m_sessions.clear();
+
+     for (auto& session: sessions["sessions"]) {
+         Session s;
+         if ( session.contains("description") ) {
+             if ( session["description"].is_string() ) {
+                s.description = session["description"].get<QString>();
+             }
+         }
+
+         if ( session.contains("last_changed") ) {
+             if ( session["last_changed"].is_string() ) {
+                s.last_changed = QDateTime::fromString( session["last_changed"].get<QString>(), "dd.MM.yyyy hh:mm:ss" );
+             }
+         }
+
+         if ( session.contains("name") ) {
+            if ( session["name"].is_string() ) {
+                s.name = session["name"].get<QString>();
+
+                // Добавление элементов в модель
+                m_sessions.push_back(s);
+             }
+         }
+    }
+
+     return true;
+}
+
+int SessionManager::currentSessionId() const
+{
+    return m_curr_session_id;
+}
+
+int SessionManager::rowCount(const QModelIndex &parent) const {
+    return m_sessions.size();
+}
+
+int SessionManager::columnCount(const QModelIndex &parent) const {
+    return kColumnCount;
+}
+
+QVariant SessionManager::data(const QModelIndex &index, int role) const
+{
+    auto row = index.row();
+    auto col = index.column();
+
+
+    if (checkIndex(index)) {
+        auto& s = m_sessions.at(row);
+
+        if (role == Qt::DisplayRole) {
+            switch (col) {
+                case kColumnName:
+                    return s.name;
+
+                case kColumnLastChanged:
+                    return s.last_changed;
+
+                case kColumnDescription: {
+                    return s.description;
+                }
+            }
+        }
+
+
+        if (role == Qt::FontRole) {
+            // TODO: выделить выбранную сессию
+        }
+    }
+
+}
+
+QVariant SessionManager::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role == Qt::DisplayRole) {
+
+        if (orientation == Qt::Horizontal) {
+            switch (section) {
+                case kColumnName:
+                    return QString(tr("Name"));
+
+                case kColumnLastChanged:
+                    return QString(tr("Changed"));
+
+                case kColumnDescription:
+                    return QString(tr("Description"));
+
+                default: break;
+            }
+        }
+        else {
+            return QString::number(section+1);
+        }
+
+    }
+
+    return QVariant();
 }
